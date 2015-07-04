4108e13ea445d4dc81cc8b7eeadcbf72926adaae
ajitborate <ajitborate@users.noreply.github.com>
2015-07-04 23:26:14 +0530

    Create includeFunc.sh

---
+++ profile/includeFunc.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+
+# fail if the file is not an executable in the path.
+inpath () {
+  ! [ $# -eq 1 ] && echo "usage: inpath <file>" && return 1
+  f="$(which "$1" 2>/dev/null)"
+  [ -f "$f" ] && return 0
+  return 1
+}
+
+#get the ip address of a host easily.
+getip () {
+  for each in "$@"; do
+    echo $each
+    echo "nslookup:"
+    nslookup $each | grep Address: | grep -v '#' | egrep -o '([0-9]+\.){3}[0-9]+'
+    echo "ping:"
+    ping -c1 -t1 $each | egrep -o '([0-9]+\.){3}[0-9]+' | head -n1
+    echo "dig:"
+    dig $each | grep . | egrep -v '^;'
+  done
+}
+
+# Show the IP addresses of this machine, with each interface that the address is on.
+ips () {
+  local interface=""
+  local types='vmnet|en|eth|vboxnet'
+  local i
+  for i in $(
+    ifconfig \
+    | egrep -o '(^('$types')[0-9]|inet (addr:)?([0-9]+\.){3}[0-9]+)' \
+    | egrep -o '(^('$types')[0-9]|([0-9]+\.){3}[0-9]+)' \
+    | grep -v 127.0.0.1
+  ); do
+    if ! [ "$( echo $i | perl -pi -e 's/([0-9]+\.){3}[0-9]+//g' )" == "" ]; then
+      interface="$i":
+    else
+      echo $interface $i
+    fi
+  done
+}
+
+# Like the ips function, but for mac addrs.
+macs () {
+  local interface=""
+  local i
+  local types='vmnet|en|eth|vboxnet'
+  for i in $(
+    ifconfig \
+    | egrep -o '(^('$types')[0-9]:|ether ([0-9a-f]{2}:){5}[0-9a-f]{2})' \
+    | egrep -o '(^('$types')[0-9]:|([0-9a-f]{2}:){5}[0-9a-f]{2})'
+  ); do
+    if [ ${i:(${#i}-1)} == ":" ]; then
+      interface=$i
+    else
+      echo $interface $i
+  fi
+  done
+}
+
+
